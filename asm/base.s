e34ae10929eb444b934c1a939702ab3f6a8e143d
Oliver Enes <oliver@eneoli.de>
2025-05-11 23:28:49 +0200

    added assembly template

---
+++ asm/base.s
@@ -0,0 +1,15 @@
+.intel_syntax noprefix
+
+.global main
+.global _main
+
+.section .text
+
+main:
+    call _main
+    mov rdi, rax  # move the return value into the first argument for the syscall
+    mov rax, 0x3C # move the exit syscall number into rax
+    syscall
+
+_main:
+# Start of generated assembly
