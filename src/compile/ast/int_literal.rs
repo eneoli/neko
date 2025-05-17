b27cf66de4fd981a20a2b5737fe3fa800f767376
Oliver Enes <oliver@eneoli.de>
2025-05-17 12:37:30 +0200

    check boundaries

--- src/compile/ast/int_literal.rs
+++ src/compile/ast/int_literal.rs
@@ -8,15 +8,30 @@ pub struct IntLiteral {
     base: u32,
 }
 
+pub const MAX_INT_DEC: u32 = 2147483648;
+pub const MAX_INT_HEX: u32 = 0xffffffff;
+
 impl IntLiteral {
     pub fn new(value: String, base: u32) -> IntLiteral {
         IntLiteral { value, base }
     }
 
     pub fn parse(&self) -> Result<u32, SemanticError> {
-        u32::from_str_radix(&self.value, self.base).map_err(|err| match err.kind() {
+        let num = u32::from_str_radix(&self.value, self.base).map_err(|err| match err.kind() {
             IntErrorKind::PosOverflow => SemanticError::IntLiteralOutOfBounds,
             _ => unreachable!("Parser returned invalid number: {}", self.value),
-        })
+        })?;
+
+        let MAX_INT = match self.base {
+            10 => MAX_INT_DEC,
+            16 => MAX_INT_HEX,
+            _ => panic!("Unknown base: {}", self.base),
+        };
+
+        if num > MAX_INT {
+            return Err(SemanticError::IntLiteralOutOfBounds);
+        }
+
+        Ok(num)
     }
 }
