cbc74accafdde81ccad4ea1e5e53b7fd62afa2ab
Oliver Enes <oliver@eneoli.de>
2025-06-05 01:41:40 +0200

    added can_have_side_effects method

--- src/compile/ast/mod.rs
+++ src/compile/ast/mod.rs
@@ -17,11 +17,46 @@ pub enum Expr {
     Binary(BinaryOp, Box<Expr>, Box<Expr>),
 }
 
+impl Expr {
+    pub fn can_have_side_effects(&self) -> bool {
+        match self {
+            Self::Ident(_, _) | Self::Int(_, _) => false,
+            Self::Unary(_, rhs) => rhs.can_have_side_effects(),
+            Self::Binary(op, lhs, rhs) => {
+                if let BinaryOp::Div = op {
+                    return true;
+                }
+
+                if let BinaryOp::Mod = op {
+                    return true;
+                }
+
+                lhs.can_have_side_effects() || rhs.can_have_side_effects()
+            }
+        }
+    }
+}
+
 #[derive(Debug, Clone)]
 pub enum Stmt {
     Decl(Type, String, Option<Expr>, SourcePos),
     Assign(String, AssignOp, Expr, SourcePos),
     Return(Expr, SourcePos),
+}
+
+impl Stmt {
+    pub fn can_have_side_effects(&self) -> bool {
+        match self {
+            Self::Decl(_, _, None, _) => false,
+            Self::Decl(_, _, Some(expr), _) => expr.can_have_side_effects(),
+            Self::Assign(_, op, expr, _) => match op {
+                AssignOp::Op(BinaryOp::Div) => true,
+                AssignOp::Op(BinaryOp::Mod) => true,
+                _ => expr.can_have_side_effects(),
+            },
+            Self::Return(expr, _) => expr.can_have_side_effects(),
+        }
+    }
 }
 
 #[derive(Debug, Clone)]
@@ -31,11 +66,20 @@ pub enum UnaryOp {
 
 #[derive(Debug, Clone)]
 pub enum BinaryOp {
-    Mul,
     Add,
     Sub,
+    Mul,
     Div,
     Mod,
+}
+
+impl BinaryOp {
+    pub fn can_have_side_effects(&self) -> bool {
+        match self {
+            Self::Div | Self::Mod => true,
+            Self::Add | Self::Sub | Self::Mul => false,
+        }
+    }
 }
 
 #[derive(Debug, Clone)]
