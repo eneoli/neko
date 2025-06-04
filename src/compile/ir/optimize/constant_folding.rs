fa387246ae238240c4af5ba181eb3003f6caf818
Oliver Enes <oliver@eneoli.de>
2025-06-05 01:45:52 +0200

    Constant Folding for Div and Mod

--- src/compile/ir/optimize/constant_folding.rs
+++ src/compile/ir/optimize/constant_folding.rs
@@ -27,7 +27,7 @@ impl ConstantFoldingOptimizer {
 
         let rhs_node = sea.node(rhs);
 
-        let NodeKind::Constant { ty, .. } = &rhs_node.kind  else {
+        let NodeKind::Constant { ty, .. } = &rhs_node.kind else {
             return node.id;
         };
 
@@ -35,18 +35,17 @@ impl ConstantFoldingOptimizer {
             return node.id;
         };
 
+        // TODO not good
         let value = match op {
-            UnaryNodeOp::Neg => -a,
+            UnaryNodeOp::Neg => i32::wrapping_neg(*a as i32),
         };
 
         let kind = NodeKind::Constant {
-            ty: Type::Int(value),
+            ty: Type::Int(value as u32),
             ctrl: sea.start(),
         };
 
-        sea.add_node(kind)
-
-
+        sea.add_node(kind, node.effects.clone())
     }
 
     fn optimize_binary(&self, node_id: NodeId, sea: &mut Sea) -> NodeId {
@@ -54,10 +53,6 @@ impl ConstantFoldingOptimizer {
         let NodeKind::Binary { ref op, lhs, rhs } = node.kind else {
             unreachable!()
         };
-
-        if op.has_side_effect() {
-            return node.id;
-        }
 
         let lhs_node = sea.node(lhs);
         let rhs_node = sea.node(rhs);
@@ -72,19 +67,25 @@ impl ConstantFoldingOptimizer {
             return node.id;
         };
 
+        // assuming side effects come from div and mod only
+        if op.has_side_effect() && i32::checked_div(*a as i32, *b as i32).is_none() {
+            return node.id;
+        }
+
         let value = match op {
-            BinaryNodeOp::Add => a + b,
-            BinaryNodeOp::Sub => a - b,
-            BinaryNodeOp::Mul => a * b,
-            _ => unreachable!(),
+            BinaryNodeOp::Add => i32::wrapping_add(*a as i32, *b as i32),
+            BinaryNodeOp::Sub => i32::wrapping_sub(*a as i32, *b as i32),
+            BinaryNodeOp::Mul => i32::wrapping_mul(*a as i32, *b as i32),
+            BinaryNodeOp::Div => i32::wrapping_div(*a as i32, *b as i32),
+            BinaryNodeOp::Mod => i32::wrapping_rem(*a as i32, *b as i32),
         };
 
         let kind = NodeKind::Constant {
-            ty: Type::Int(value),
+            ty: Type::Int(value as u32),
             ctrl: sea.start(),
         };
 
-        sea.add_node(kind)
+        sea.add_node(kind, node.effects.clone())
     }
 }
 
