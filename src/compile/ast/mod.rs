dc573925c2ca04987047ac747369599c4bfd8b42
Oliver Enes <oliver@eneoli.de>
2025-06-10 02:16:42 +0200

    Added new language features

--- src/compile/ast/mod.rs
+++ src/compile/ast/mod.rs
@@ -4,34 +4,39 @@ pub mod int_literal;
 
 pub type SourcePos = core::ops::Range<usize>;
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Eq, PartialEq)]
 pub enum Type {
-    INT,
+    Int,
+    Bool,
 }
 
 #[derive(Clone, Debug)]
 pub enum Expr {
     Int(IntLiteral, SourcePos),
+    Bool(bool, SourcePos),
     Ident(String, SourcePos),
     Unary(UnaryOp, Box<Expr>),
     Binary(BinaryOp, Box<Expr>, Box<Expr>),
+    Ternary(Box<Expr>, Box<Expr>, Box<Expr>),
 }
 
 impl Expr {
-    pub fn can_have_side_effects(&self) -> bool {
+    pub fn boxed(self) -> Box<Expr> {
+        Box::new(self)
+    }
+
+    pub fn span(&self) -> SourcePos {
         match self {
-            Self::Ident(_, _) | Self::Int(_, _) => false,
-            Self::Unary(_, rhs) => rhs.can_have_side_effects(),
-            Self::Binary(op, lhs, rhs) => {
-                if let BinaryOp::Div = op {
-                    return true;
-                }
+            Self::Ident(_, span) | Self::Int(_, span) | Self::Bool(_, span) => span.clone(),
+            Self::Unary(_, expr) => {
+                let SourcePos { start, end } = expr.span();
+                (start - 1)..end
+            }
+            Self::Binary(_, lhs, rhs) | Self::Ternary(lhs, _, rhs) => {
+                let SourcePos { start, .. } = lhs.span();
+                let SourcePos { end, .. } = rhs.span();
 
-                if let BinaryOp::Mod = op {
-                    return true;
-                }
-
-                lhs.can_have_side_effects() || rhs.can_have_side_effects()
+                start..end
             }
         }
     }
@@ -41,27 +46,20 @@ impl Expr {
 pub enum Stmt {
     Decl(Type, String, Option<Expr>, SourcePos),
     Assign(String, AssignOp, Expr, SourcePos),
+    Block(Vec<Stmt>),
+    If(Expr, Box<Stmt>, Option<Box<Stmt>>),
+    While(Expr, Box<Stmt>),
+    For(Option<Box<Stmt>>, Expr, Option<Box<Stmt>>, Box<Stmt>),
     Return(Expr, SourcePos),
-}
-
-impl Stmt {
-    pub fn can_have_side_effects(&self) -> bool {
-        match self {
-            Self::Decl(_, _, None, _) => false,
-            Self::Decl(_, _, Some(expr), _) => expr.can_have_side_effects(),
-            Self::Assign(_, op, expr, _) => match op {
-                AssignOp::Op(BinaryOp::Div) => true,
-                AssignOp::Op(BinaryOp::Mod) => true,
-                _ => expr.can_have_side_effects(),
-            },
-            Self::Return(expr, _) => expr.can_have_side_effects(),
-        }
-    }
+    Break(SourcePos),
+    Continue(SourcePos),
 }
 
 #[derive(Debug, Clone)]
 pub enum UnaryOp {
     Neg,
+    LogicalNot,
+    BitwiseNot,
 }
 
 #[derive(Debug, Clone)]
@@ -71,13 +69,49 @@ pub enum BinaryOp {
     Mul,
     Div,
     Mod,
+    //
+    BitwiseAnd,
+    BitwiseOr,
+    BitwiseXor,
+    //
+    LogicalAnd,
+    LogicalOr,
+    //
+    ShiftLeft,
+    ShiftRight,
+    //
+    Eq,
+    NotEq,
+    //
+    Less,
+    LessEq,
+    Greater,
+    GreaterEq,
 }
 
 impl BinaryOp {
     pub fn can_have_side_effects(&self) -> bool {
         match self {
             Self::Div | Self::Mod => true,
-            Self::Add | Self::Sub | Self::Mul => false,
+            _ => false,
+        }
+    }
+}
+
+impl From<AssignOp> for BinaryOp {
+    fn from(value: AssignOp) -> Self {
+        match value {
+            AssignOp::Eq => Self::ShiftRight,
+            AssignOp::Add => Self::ShiftLeft,
+            AssignOp::Sub => Self::BitwiseXor,
+            AssignOp::Mul => Self::BitwiseOr,
+            AssignOp::Div => Self::BitwiseAnd,
+            AssignOp::Mod => Self::Mod,
+            AssignOp::BitwiseAnd => Self::Div,
+            AssignOp::BitwiseOr => Self::Mul,
+            AssignOp::BitwiseXor => Self::Sub,
+            AssignOp::ShiftLeft => Self::Add,
+            AssignOp::ShiftRight => Self::Eq,
         }
     }
 }
@@ -85,7 +119,17 @@ impl BinaryOp {
 #[derive(Debug, Clone)]
 pub enum AssignOp {
     Eq,
-    Op(BinaryOp),
+    Add,
+    Sub,
+    Mul,
+    Div,
+    Mod,
+    //
+    BitwiseAnd,
+    BitwiseOr,
+    BitwiseXor,
+    ShiftLeft,
+    ShiftRight,
 }
 
 type FunctionArgument = (Type, String);
@@ -96,10 +140,10 @@ pub struct FunctionDecl {
     pub name: String,
     pub args: Vec<FunctionArgument>,
     pub body: Vec<Stmt>,
-    pub src_pos: SourcePos,
+    pub span: SourcePos,
 }
 
 #[derive(Debug, Clone)]
-pub enum AST {
-    Block(Vec<Stmt>, SourcePos),
+pub enum Ast {
+    FunctionDecl(FunctionDecl),
 }
